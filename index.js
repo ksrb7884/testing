1ec33753030295aa9d3446a51e8eff05e15fdc83
Karan Singh <ksrb7884@gmail.com>
2025-04-23 00:57:19 +0530

    setMyName function added

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const age = 23
 function sayHello() {
     console.log(name,age)
 }
+
+
+console.log(sayHello())
+
+function setMyName() {
+    console.log("My name is Karan")
+}
+
+console.log(setMyName())
